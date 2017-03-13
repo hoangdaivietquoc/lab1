89bc5dab1df29627dce39a4c4dae39a6ff86acc1
daivietquochoang <hoangdaivietquoc@gmail.com>
2017-03-13 17:14:48 +0100

    the first time

--- src/main/java/com/polytech/repository/PostRepository.java
+++ src/main/java/com/polytech/repository/PostRepository.java
@@ -2,20 +2,13 @@ package com.polytech.repository;
 
 import com.polytech.business.Post;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * Created by daivietquochoang on 13/03/2017.
  */
-public class PostRepository {
+public interface PostRepository {
+    public void save(Post post);
 
-    List<Post> db = new ArrayList<Post>();
-    public void save(Post post) {
-        this.db.add(post);
-    }
-
-    public List<Post> findAll() {
-        return db;
-    }
+    public List<Post> findAll();
 }
--- src/main/java/com/polytech/view/Main.java
+++ src/main/java/com/polytech/view/Main.java
@@ -3,6 +3,7 @@ package com.polytech.view;
 import com.polytech.business.Post;
 import com.polytech.business.PublicationService;
 import com.polytech.repository.PostRepository;
+import com.polytech.repository.PostRepositoryImpl;
 
 import java.util.List;
 
@@ -13,7 +14,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("coucou polytech");
-        PostRepository repository = new PostRepository();
+        PostRepository repository = new PostRepositoryImpl();
         PublicationService publicationService = new PublicationService(repository);
         Post post = new Post("Luminy est cool");
         publicationService.post(post);
