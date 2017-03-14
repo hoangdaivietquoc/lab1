ce4aa1cf5b6e3ab2a5d160a69910117dc85ce145
daivietquochoang <hoangdaivietquoc@gmail.com>
2017-03-14 12:05:43 +0100

    new class ApplicationConfig

--- src/main/java/com/polytech/config/ApplicationConfig.java
+++ src/main/java/com/polytech/config/ApplicationConfig.java
@@ -5,11 +5,12 @@ import com.polytech.business.PublicationServiceImpl;
 import com.polytech.repository.JdbcPostRepository;
 import com.polytech.repository.PostRepository;
 import org.apache.commons.dbcp.BasicDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
-import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 import javax.sql.DataSource;
 
@@ -18,19 +19,33 @@ import javax.sql.DataSource;
  * Created by daivietquochoang on 13/03/2017.
  */
 @Configuration
+@PropertySource("classpath:/application.properties")
 public class ApplicationConfig {
-//    @Bean
-//    public DataSource dataSource(){
-//        return new EmbeddedDatabaseBuilder().setType(EmbeddedDatabaseType.H2).addScript("create-schema.sql").build();
-//    }
-
+    @Autowired
+    private Environment environment;
+//    @Value("${datasource.driverName}")
+//    private String driverClassName;
+//
+//    @Value("${datasource.url}")
+//    private String url;
+//
+//    @Value("${datasource.username}")
+//    private String username;
+//
+//    @Value("${datasource.password}")
+//    private String password;
     @Bean
     public DataSource dataSource(){
+        String driverClassName = environment.getProperty("datasource.driverName");
+        String url = environment.getProperty("datasource.url");
+        String username = environment.getProperty("datasource.username");
+        String password = environment.getProperty("datasource.password");
+
         BasicDataSource dataSource = new BasicDataSource();
-        dataSource.setUsername("root");
-        dataSource.setPassword("");
-        dataSource.setUrl("jdbc:mysql://localhost:3306/arc_app_entr");
-        dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        dataSource.setUrl(url);
+        dataSource.setDriverClassName(driverClassName);
         return dataSource;
     }
 
